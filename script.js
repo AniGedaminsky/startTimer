8ced7d36c46081bcaf9970a3c1262b6a4abac8ba
Ani Gedaminski <160894666+AniGedaminsky@users.noreply.github.com>
2024-04-15 17:47:09 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function startTimer() {
+    var timeInput = document.getElementById('time').value;
+    var timerDisplay = document.getElementById('Timer');
+    var timeLeft = timeInput;
+    console.log(timeInput, timeLeft);
+    
+    var countDown = setInterval(function() {
+        timeLeft--;
+        timerDisplay.innerHTML = 'Time Left: ' + timeLeft + ' seconds';
+        
+        if (timeLeft <= 0) {
+            clearInterval(countDown);
+            timerDisplay.innerHTML = "Time is up!";
+        }
+    }, 1000);
+}
